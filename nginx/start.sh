cfc66c9e4c69e3ec5cf6238fd894681056188979
selim13 <selim013@gmail.com>
2017-03-26 13:38:17 +0300

    Add env variable to set hostname for nginx

--- nginx/start.sh
+++ nginx/start.sh
@@ -2,6 +2,7 @@
 
 cat /etc/nginx/default_template.conf \
     | sed "s/\${BX_UPSTREAM}/${BX_UPSTREAM}/g" \
+    | sed "s/\${BX_HOSTNAME}/${BX_HOSTNAME}/g" \
     | sed "s/\${BX_ROOT}/${BX_ROOT}/g" > /etc/nginx/conf.d/default.conf
 
 exec nginx -g "daemon off;"
